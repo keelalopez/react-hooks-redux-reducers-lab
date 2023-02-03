ed28214e4698ee2eac2c902ea502fb5eeff9fa81
Keila Lopez <keilalofra@gmail.com>
2023-02-02 23:17:36 -0600

    commit tree 4b8a46be604c372df3d06b24f468e8b2d2bf2b3e

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,19 @@
+// const initialState = { friends: [] };
+
 export function manageFriends(state, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {
+        // using spread operator to create a new array with what was existing and adding the action's object stored in payload
+        friends: [...state.friends, action.payload],
+      };
+    case "friends/remove":
+      return {
+        // for this action, the payload is an id 
+        friends: state.friends.filter((friend) => friend.id !== action.payload),
+      };
+    default :
+      return state;
+  }
 }
